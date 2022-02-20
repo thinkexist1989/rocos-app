590c1c35febdcfd5dd3f99986c395ea6536704e2
thinkexist1989 <992534544@qq.com>
2022-02-20 12:23:59 +0800

    change file name
    根据Google c++规范修改文件名

---
+++ include/kinematics.h
@@ -0,0 +1,40 @@
+// Copyright 2021, Yang Luo"
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @Author
+// Yang Luo, PHD
+// Shenyang Institute of Automation, Chinese Academy of Sciences.
+// email: luoyang@sia.cn
+
+#ifndef ROCOS_APP_KINEMATICS_H
+#define ROCOS_APP_KINEMATICS_H
+
+#include <urdf_parser/urdf_parser.h>
+#include <urdf_model/model.h>
+
+#include <kdl/frames.hpp>
+#include <kdl/chain.hpp>
+
+#include <trac_ik/trac_ik.hpp> //逆运动学处理
+
+namespace rocos {
+
+    class Kinematics {
+
+    };
+
+}
+
+
+#endif //ROCOS_APP_KINEMATICS_H
---
+++ src/kinematics.cc
@@ -0,0 +1,10 @@
+//
+// Created by think on 2022/2/20.
+//
+
+#include "kinematics.h"
+
+
+namespace rocos {
+
+}
