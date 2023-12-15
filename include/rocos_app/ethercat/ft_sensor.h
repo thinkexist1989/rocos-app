83f389a03c3eae359ce2b3f9d64977ddf9be10b7
Yang Luo <992534544@qq.com>
2023-12-15 17:55:26 +0800

    Add Ft_Sensor

---
+++ include/rocos_app/ethercat/ft_sensor.h
@@ -0,0 +1,16 @@
+//
+// Created by think on 2023/12/15.
+//
+
+#ifndef ROCOS_APP_FT_SENSOR_H
+#define ROCOS_APP_FT_SENSOR_H
+
+namespace rocos {
+
+    class FtSensor {
+
+    };
+
+} // rocos
+
+#endif //ROCOS_APP_FT_SENSOR_H
---
+++ src/ft_sensor.cc
@@ -0,0 +1,10 @@
+//
+// Created by think on 2023/12/15.
+//
+
+#include <rocos_app/ethercat/ft_sensor.h>
+
+namespace rocos {
+
+
+} // rocos
