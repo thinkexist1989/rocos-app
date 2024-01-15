2d1a1aba83cee151080e4f853b4aa8458635946c
JinCheng Sun <1962945668@qq.com>
2024-01-15 16:34:49 +0800

    修改了yaml的路径
    修改了yaml的读取路径

--- include/rocos_app/JC_helper_dynamics.hpp
+++ include/rocos_app/JC_helper_dynamics.hpp
@@ -144,7 +144,7 @@ namespace JC_helper
         std::vector<double> joint_collision;
         bool flag_collision_detection;
         KDL::Chain robotchain;
-        admittance_joint(rocos::Robot *robot_ptr, std::string yaml_path = "joint_impedance_control.yaml");
+        admittance_joint(rocos::Robot *robot_ptr, std::string yaml_path = "config/joint_impedance_control.yaml");
         ~admittance_joint();
         // 初始化参数，从yaml文件中读取及初始化q_min,q_max,因为从yaml中读取，所以不需要各个参数的set函数
         int init(string yaml_path);
